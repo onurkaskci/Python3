9538ecc16de9e670d3ff833e69a04d2f8fd3d01f
onurkaskci <onurka.46@gmail.com>
2023-01-02 14:51:51 +0300

    n

--- python shorts/lambda.py
+++ python shorts/lambda.py
@@ -8,15 +8,3 @@ print(f(g(h(x))))
 # A: 21
 # B: 201
 # C: 11
-
-
-f = lambda x: x + 1
-g = lambda x: x * 2
-h = lambda x: x ** 2
-
-x = 10
-print((f(g(h(x)))) + (g(f(h(x)))))
-
-# A: 403
-# B: 211
-# C: 102
