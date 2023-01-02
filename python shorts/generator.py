37d0445d1571521c8495c191e0feec3d484b3c95
onurkaskci <onurka.46@gmail.com>
2023-01-02 14:08:26 +0300

    calculating the bytes

--- python shorts/generator.py
+++ python shorts/generator.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 def count_up_to(max):
     count = 1
@@ -14,21 +15,5 @@ def count_up_to_list(max):
         count += 1
     return count_list
 
-start_time_gen = time.perf_counter()
-# Generate values using the generator
-for number in count_up_to(1000000):
-    pass
-end_time_gen = time.perf_counter()
-elapsed_time_gen = end_time_gen - start_time_gen
-
-start_time_normal = time.perf_counter()
-# Generate values using the traditional function
-for number in count_up_to_list(1000000):
-    pass
-end_time_normal = time.perf_counter()
-elapsed_time_normal = end_time_normal - start_time_normal
-
-# Multiple the seconds by 1000 to get milliseconds
-print(f"Elapsed time for Generator: {elapsed_time_gen:.6f} seconds") 
-print(f"Elapsed time for Return: {elapsed_time_normal:.6f} seconds")
-print(f'Generator was {(elapsed_time_normal-elapsed_time_gen)*1000:.0f} (milli)seconds faster!')
+print(f'Generator: {sys.getsizeof(count_up_to(1000000))} bytes')
+print(f'Regular: {sys.getsizeof(count_up_to_list(1000000))} bytes')
