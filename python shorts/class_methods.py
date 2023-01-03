c75bebb3a8de7fc6f6671805d63b231b65db9264
onurkaskci <onurka.46@gmail.com>
2023-01-03 19:07:41 +0300

    a

--- python shorts/class_methods.py
+++ python shorts/class_methods.py
@@ -3,15 +3,3 @@ class Circle:
     def __init__(self, radius):
         self.radius = radius
 
-    def circumference(self):
-        return 2 * 3.14 * self.radius
-
-    def area(self):
-        return 3.14 * self.radius * self.radius
-
-circle = Circle(3)
-
-print(circle.circumference())  # Output: 18.84955592153876
-
-# Call the area method
-print(circle.area())  # Output: 28.274333882308138
