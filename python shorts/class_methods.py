c5284096d78d715c222fe2adf09ff22dd9e9b5c8
onurkaskci <onurka.46@gmail.com>
2023-01-03 19:08:36 +0300

    aas

--- python shorts/class_methods.py
+++ python shorts/class_methods.py
@@ -3,3 +3,9 @@ class Circle:
     def __init__(self, radius):
         self.radius = radius
 
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
